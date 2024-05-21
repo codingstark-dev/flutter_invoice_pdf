9c8fe806c870b9da91af542425264be93a2276af
“Himanshu” <mhimanshu.338@gmail.com>
2024-05-21 22:41:46 +0530

    feat: Create SplashScreen with 3-second delay and navigate to onboarding screen

---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,35 @@
+//create splash screen for 3 seconds
+//       home: SplashScreen(),
+//     );
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:invoice_pdf_generate/style/ConstStyle.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () {
+      Get.offAndToNamed('/onboarding');
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Image.asset(
+          'assets/splash.png',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
