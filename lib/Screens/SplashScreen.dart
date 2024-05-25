c82d80deb0aab355e13f103eaa2c22ef12a7bf3f
“Himanshu” <mhimanshu.338@gmail.com>
2024-05-26 03:29:48 +0530

    chore: Update file handling and error handling in SplashScreen.dart and file_handle_api.dart

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -14,7 +14,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Get.offAndToNamed('/onboarding');
     });
     super.initState();
--- lib/file_handle_api.dart
+++ lib/file_handle_api.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 import 'package:downloadsfolder/downloadsfolder.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
 import 'package:invoice_pdf_generate/Utils/PermissionUtil.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:path_provider/path_provider.dart';
 import 'package:open_file/open_file.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class FileHandleApi {
   // save pdf file function
@@ -12,31 +13,45 @@ class FileHandleApi {
     required String name,
     required pw.Document pdf,
   }) async {
-    await storagePermission();
-
-    Directory _directory = Directory("");
-    Directory downloadDirectory = await getDownloadDirectory();
-
-    if (Platform.isAndroid) {
-      // Redirects it to download folder in android
-      _directory = downloadDirectory;
-    } else {
-      _directory = await getApplicationDocumentsDirectory();
-    }
-    final exPath = _directory.path;
+    try {
+  await storagePermission();
+  
+  Directory directory = Directory("");
+  Directory downloadDirectory = await getDownloadDirectory();
+  
+  if (Platform.isAndroid) {
+    // Redirects it to download folder in android
+    directory = downloadDirectory;
+  } else {
+    directory = await getApplicationDocumentsDirectory();
+  }
+  final exPath = directory.path;
+  if (kDebugMode) {
     print("Saved Path: $exPath");
-    await Directory(exPath).create(recursive: true);
-    final bytes = await pdf.save();
-
-    // final dir = await getApplicationDocumentsDirectory();
-    // final dir = await getExternalStorageDirectory();
-    final file = File("$exPath/$name");
-    await file.writeAsBytes(bytes);
-    // await openDownloadFolder();
-   await openFile(file);
-
-    //open pdf file
-    return file;
+  }
+  await Directory(exPath).create(recursive: true);
+  final bytes = await pdf.save();
+  
+  // final dir = await getApplicationDocumentsDirectory();
+  // final dir = await getExternalStorageDirectory();
+  final file = File("$exPath/$name");
+  await file.writeAsBytes(bytes);
+  // await openDownloadFolder();
+     await openFile(file);
+  
+  //open pdf file
+  return file;
+} on Exception catch (e) {
+    Get.snackbar(
+    'Error',
+    e.toString(),
+    snackPosition: SnackPosition.BOTTOM,
+  );
+  if (kDebugMode) {
+    print('error: $e');
+  }
+  rethrow;
+}
   }
 
 static Future<File> saveTempDocument({
@@ -50,31 +65,56 @@ static Future<File> saveTempDocument({
     // final exPath = _directory.path;
     // print("Saved Path: $exPath");
     // await Directory(exPath).create(recursive: true);
-    final bytes = await pdf.save();
-
-    final dir = await getApplicationDocumentsDirectory();
-    // final dir = await getExternalStorageDirectory();
-    final file = File("${dir.path}/$name");
-    await file.writeAsBytes(bytes);
-    // await openDownloadFolder();
-   // await openFile(file);
-
-    //open pdf file
-    return file;
+    try {
+  final bytes = await pdf.save();
+  
+  final dir = await getApplicationDocumentsDirectory();
+  // final dir = await getExternalStorageDirectory();
+  final file = File("${dir.path}/$name");
+  await file.writeAsBytes(bytes);
+  // await openDownloadFolder();
+     // await openFile(file);
+  
+  //open pdf file
+  return file;
+} on Exception catch (e) {
+      Get.snackbar(
+    'Error',
+    e.toString(),
+    snackPosition: SnackPosition.BOTTOM,
+  );
+  if (kDebugMode) {
+    print('error: $e');
+  }
+  rethrow;
+}
   }
   static Future<File> saveDocumentTolocal({
     required String name,
     required pw.Document pdf,
   }) async {
     // await requestPermission();
-    final bytes = await pdf.save();
-
-    // final dir = await getApplicationDocumentsDirectory();
-    final dir = await getTemporaryDirectory();
-    final file = File('${dir.path}/$name');
-    await file.writeAsBytes(bytes);
-    return file;
+    try {
+  final bytes = await pdf.save();
+  
+  // final dir = await getApplicationDocumentsDirectory();
+  final dir = await getTemporaryDirectory();
+  final file = File('${dir.path}/$name');
+  await file.writeAsBytes(bytes);
+  return file;
+} on Exception catch (e) {
+    Get.snackbar(
+    'Error',
+    e.toString(),
+    snackPosition: SnackPosition.BOTTOM,
+  );
+  if (kDebugMode) {
+    print('error: $e');
   }
+  rethrow;
+}
+  }
+
 
   // open pdf file function
   static Future openFile(File file) async {
@@ -87,7 +127,14 @@ static Future<File> saveTempDocument({
         type: 'application/pdf',
       );
     } on Exception catch (e) {
-      print('error: $e');
+      Get.snackbar(
+        'Error',
+       e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+      if (kDebugMode) {
+        print('error: $e');
+      }
     }
   }
 }
