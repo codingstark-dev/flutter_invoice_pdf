5fa057805b58bc5685e5033328f0ba5dafc86120
“Himanshu” <mhimanshu.338@gmail.com>
2024-06-13 22:23:14 +0530

    chore: Add rewardAds constant in Ads.dart

--- lib/Utils/Ads.dart
+++ lib/Utils/Ads.dart
@@ -1,2 +1,3 @@
 const String bannerAds = "ca-app-pub-9164317534746006/6297718291";
 const String interstitialAds = "ca-app-pub-9164317534746006/4058120657"; 
+const String rewardAds = "ca-app-pub-9164317534746006/4058120657";
